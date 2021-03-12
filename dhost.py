fb20cf85bf535621254b216b5c75ed23e5b80925
2O4 <35725720+2O4@users.noreply.github.com>
2021-03-12 15:12:28 +0100

    OAuth2 Resource owner password-based flow to get a token

--- dhost.py
+++ dhost.py
@@ -4,29 +4,36 @@ import argparse
 import requests
 from getpass import getpass
 
-
+OAUTH_SERVER_URL = 'http://127.0.0.1:8000/'
+OAUTH_TOKEN_URL = 'oauth2/token/'
+OAUTH_CLIENT_ID = 'test'
 DEFAULT_API_URL = 'http://127.0.0.1:8000/'
-TOKEN_AUTH_URL = 'token-auth/'
 TOKEN_PREFIX = 'Token'
 
 
 class DhostAPI:
-    """A common class to call the Dhost API,
-    You should subclass this to implement call to the API."""
+    """
+    A common class to call the Dhost API,
+    You should subclass this to implement call to the API.
+    """
 
     def __init__(
         self,
         token=None,
         username=None,
-        API_URL=DEFAULT_API_URL,
-        TOKEN_AUTH_URL = TOKEN_AUTH_URL,
+        OAUTH_SERVER_URL=OAUTH_SERVER_URL,
+        OAUTH_TOKEN_URL=OAUTH_TOKEN_URL,
+        OAUTH_CLIENT_ID=OAUTH_CLIENT_ID,
         TOKEN_PREFIX=TOKEN_PREFIX,
+        API_URL=DEFAULT_API_URL,
     ):
         self.username = username
         self.token = token
-        self.API_URL = API_URL
-        self.TOKEN_AUTH_URL = TOKEN_AUTH_URL
+        self.OAUTH_SERVER_URL = OAUTH_SERVER_URL
+        self.OAUTH_TOKEN_URL = OAUTH_TOKEN_URL
+        self.OAUTH_CLIENT_ID = OAUTH_CLIENT_ID
         self.TOKEN_PREFIX = TOKEN_PREFIX
+        self.API_URL = API_URL
 
         if self.token is None:
             self.authentify()
@@ -37,12 +44,23 @@ class DhostAPI:
         self.get_token_auth()
 
     def get_token_auth(self):
-        url = self.API_URL + self.TOKEN_AUTH_URL
+        """
+        With the `Resource owner password-based` on a public client type
+        this function will authenticate the user from his credentials and get
+        a token to access the API
+        """
+        url = self.OAUTH_SERVER_URL + self.OAUTH_TOKEN_URL
         password = getpass('password: ')
-        cred = {'username': self.username, 'password': password}
+        cred = {
+            'username': self.username,
+            'password': password,
+            'client_id': self.OAUTH_CLIENT_ID,
+            'grant_type': 'password'
+        }
         r = requests.post(url, data=cred)
         if r.status_code == 200:
-            self.token = r.json()['token']
+            print(r.json())
+            self.token = r.json()['access_token']
             return self.token
         else:
             print('Authentication failure, code:', r.status_code)
@@ -59,7 +77,10 @@ class DhostAPI:
         """Prepare and API request with authorization header and build URL"""
         if url is None:
             if uri is None:
-                raise Exception('You must provide either an URL or an URI to make an API request.')
+                raise Exception(
+                    'You must provide either an URL or an URI to make an API '
+                    'request.'
+                )
             else:
                 url = self.API_URL + uri
         # TODO add passed headers params
@@ -68,32 +89,44 @@ class DhostAPI:
 
     def post(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a POST request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.post(url, headers=headers, *args, **kwargs)
 
     def get(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a GET request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.get(url, headers=headers, *args, **kwargs)
 
     def put(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a PUT request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.put(url, headers=headers, *args, **kwargs)
 
     def patch(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a PUT request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.patch(url, headers=headers, *args, **kwargs)
 
     def delete(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a DELETE request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.delete(url, headers=headers, *args, **kwargs)
 
     def head(self, uri=None, url=None, headers=None, *args, **kwargs):
         """Send a HEAD request to the API"""
-        url, headers = self._prepare_api_request(url=url, uri=uri, headers=headers)
+        url, headers = self._prepare_api_request(
+            url=url, uri=uri, headers=headers
+        )
         return requests.head(url, *args, **kwargs)
 
     def post_file(self, file_paht, *args, **kwargs):
@@ -103,6 +136,7 @@ class DhostAPI:
 
 
 class AppManagement(DhostAPI):
+
     def list_apps(self):
         print('listing apps')
         uri = 'apps/'
@@ -139,40 +173,49 @@ class AppManagement(DhostAPI):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='dhost', description='%(prog)s CLI tool to host decentralized websites.')
+    parser = argparse.ArgumentParser(
+        prog='dhost',
+        description='%(prog)s CLI tool to host decentralized websites.'
+    )
 
-    parser.add_argument('-u', '--username', help="Connect to API with username and password.")
+    parser.add_argument(
+        '-u', '--username', help="Connect to API with username and password."
+    )
     parser.add_argument('-t', '--token', help="Connect to API with token.")
-    parser.add_argument('-T', '--get-token', action='store_true', help="Get your API token from username and password.")
+    parser.add_argument(
+        '-T',
+        '--get-token',
+        action='store_true',
+        help="Get your API token from username and password."
+    )
     parser.add_argument('-a', '--api_url', default=DEFAULT_API_URL)
-    #parser.add_argument('INPUT_FILE', help='file to upload', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument(
+        'INPUT_FILE',
+        help='file to upload',
+        type=argparse.FileType('r'),
+        default=sys.stdin
+    )
 
     subparser = parser.add_subparsers(dest='cmd')
 
     app = subparser.add_parser('app', help="Manage you apps")
     app_sub = app.add_subparsers(dest='app_cmd')
-
     # app list
     list_app = app_sub.add_parser('list', help="List apps.")
-
     # app create
     create_app = app_sub.add_parser('create', help="Create a new app.")
     create_app.add_argument('app_name')
-
     # app infos
     detail_app = app_sub.add_parser('infos', help="Get details about and app.")
     detail_app.add_argument('app_name')
-
     # app update
     update_app = app_sub.add_parser('update', help="Update and app.")
     update_app.add_argument('app_name')
-
     # app delete
     delete_app = app_sub.add_parser('delete', help="Delete an app.")
     delete_app.add_argument('app_name')
 
     args = parser.parse_args()
-
 
     if args.get_token:
         print('Token: ' + instance.get_token())
@@ -183,21 +226,20 @@ if __name__ == '__main__':
             username=args.username,
             API_URL=args.api_url,
         )
-    elif args.cmd=='app':
+    elif args.cmd == 'app':
         app_cmd = args.app_cmd
         instance = AppManagement(
             token=args.token,
             username=args.username,
             API_URL=args.api_url,
         )
-        if app_cmd=='list':
+        if app_cmd == 'list':
             instance.list_apps()
-        elif app_cmd=='infos':
+        elif app_cmd == 'infos':
             instance.read_app(args.app_name)
-        elif app_cmd=='update':
+        elif app_cmd == 'update':
             instance.update_app(args.app_name)
-        elif app_cmd=='create':
+        elif app_cmd == 'create':
             instance.create_app(args.app_name)
-        elif app_cmd=='delete':
+        elif app_cmd == 'delete':
             instance.delete_app(args.app_name)
-
